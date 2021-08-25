b41b071ea434f710101e53da2cfb0996d027549f
oramm <marek@envi.com.pl>
2021-08-25 22:31:01 +0200

    cors

--- docs/GAuth2.js
+++ docs/GAuth2.js
@@ -96,7 +96,7 @@ class GAuth2 {
         let result = await fetch(MainSetup.serverUrl + 'login', {
             method: 'POST',
             credentials: 'include',
-            mode: 'no-cors',
+            mode: 'cors',
             headers: myHeaders,
 
             withCredentials: true,
